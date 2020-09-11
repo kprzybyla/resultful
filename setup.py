16c53035a49a7a845f3451ac5aa3524a06c9bcfe
Krzysztof Przybyła <krzysztof.h.przybyla@gmail.com>
2020-09-11 23:29:08 +0200

    Better CI, metadata in setup.cfg, minor tweaks

--- setup.py
+++ setup.py
@@ -1,93 +1,35 @@
-import os
-import codecs
-
-from typing import (
-    Final,
-)
-
 from setuptools import (
     setup,
-    find_packages,
 )
-
-BLACK: Final[str] = "black == 20.8b1"
-FLAKE8: Final[str] = "flake8 ~= 3.8.0"
-MYPY: Final[str] = "mypy @ git+https://github.com/kprzybyla/mypy.git@testplates"
-
-PYTEST: Final[str] = "pytest ~= 6.0.1"
-PYTEST_COV: Final[str] = "pytest-cov ~= 2.8.0"
-HYPOTHESIS: Final[str] = "hypothesis ~= 5.6.0"
-
-SPHINX: Final[str] = "sphinx ~= 3.0.0"
-SPHINX_RTD_THEME: Final[str] = "sphinx_rtd_theme ~= 0.4.3"
-SPHINX_AUTODOC_TYPEHINTS: Final[str] = "sphinx_autodoc_typehints ~= 1.10.0"
-
-WHEEL: Final[str] = "wheel"
-TWINE: Final[str] = "twine"
 
 
 def main():
-    with codecs.open("README.rst", encoding="utf-8") as handle:
-        long_description = handle.read()
-
     setup(
-        name="resultful",
-        author="Krzysztof Przybyła",
-        url="https://github.com/kprzybyla/resultful",
-        description="Expect results, not exceptions!",
-        long_description=long_description,
-        classifiers=[
-            "Development Status :: 3 - Alpha",
-            "Intended Audience :: Developers",
-            "Natural Language :: English",
-            "Operating System :: POSIX",
-            "Programming Language :: Python :: 3.8",
-            "Topic :: Software Development :: Testing",
-            "Topic :: Software Development :: Libraries",
-        ],
-        python_requires="~= 3.8",
-        extras_require={
-            "black": [
-                BLACK,
-            ],
-            "lint": [
-                FLAKE8,
-            ],
-            "mypy": [
-                MYPY,
-                PYTEST,
-                HYPOTHESIS,
-            ],
-            "test": [
-                PYTEST,
-                PYTEST_COV,
-                HYPOTHESIS,
-            ],
-            "docs": [
-                SPHINX,
-                SPHINX_RTD_THEME,
-                SPHINX_AUTODOC_TYPEHINTS,
-            ],
-            "deploy": [
-                WHEEL,
-                TWINE,
-            ],
-        },
-        use_scm_version={
-            "write_to": os.path.join("src/resultful/__version__.py"),
-        },
-        platforms=[
-            "linux",
-        ],
+        python_requires=">= 3.8",
         setup_requires=[
+            "setuptools",
             "setuptools_scm",
         ],
-        packages=find_packages(where="src"),
-        package_dir={
-            "": "src",
-        },
-        package_data={
-            "resultful": ["py.typed"],
+        extras_require={
+            "format": [
+                "black == 20.8b1",
+                "flake8 ~= 3.8.0",
+            ],
+            "test": [
+                "mypy @ git+https://github.com/kprzybyla/mypy.git@testplates",
+                "pytest ~= 6.0.1",
+                "pytest-cov ~= 2.8.0",
+                "hypothesis ~= 5.6.0",
+            ],
+            "docs": [
+                "sphinx ~= 3.0.0",
+                "sphinx_rtd_theme ~= 0.4.3",
+                "sphinx_autodoc_typehints ~= 1.10.0",
+            ],
+            "deploy": [
+                "wheel",
+                "twine",
+            ],
         },
     )
 
